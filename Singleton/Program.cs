d2df818d9723eed1268c3ed28377c241d9c27d90
Sidney Cesar <scesar@bcompany.com.br>
2018-12-18 13:58:42 -0200

    Singleton

---
+++ Singleton/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var instance1 = Singleton.Instance;
+                var instance2 = Singleton.Instance;
+
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Singleton();
+
+                    Console.WriteLine("Object instanced");
+                }
+                else
+                {
+                    Console.WriteLine("Object already instanced");
+                }
+
+                return instance;
+            }
+        }
+    }
+}
