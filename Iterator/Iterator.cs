956d3430ccc2af9f6540035c4bf908ea74202482
Sidney Cesar <scesar@bcompany.com.br>
2018-12-19 14:07:00 -0200

    Interator

---
+++ Iterator/Iterator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Iterator
+{
+    public class IteratorExample 
+    {
+        Dictionary<string, string> _dictionary;
+
+        public IteratorExample()
+        {
+            // Add 5 names to a key/value pair list.
+            _dictionary = new Dictionary<string, string>();
+            _dictionary.Add("Hans", "Pisces");
+            _dictionary.Add("Fred", "Aquarius");
+            _dictionary.Add("Andrew", "Gemini");
+            _dictionary.Add("Zach", "Scorpio");
+            _dictionary.Add("Berfa", "Cancer");
+        }
+
+    }
+}
---
+++ Iterator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IteratorExample example = new IteratorExample();
+
+                foreach (string item in example)
+                    Console.WriteLine(item);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
