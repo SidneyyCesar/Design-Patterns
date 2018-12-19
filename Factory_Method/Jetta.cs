3763694e7649e58ef8aa4838a7ef2f7581ce4443
Sidney Cesar <scesar@bcompany.com.br>
2018-12-19 10:51:24 -0200

    Factory Method

---
+++ Factory_Method/Jetta.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FactoryMethod
+{
+    public class Jetta : ICarro
+    {
+        public void ShowDetails()
+        {
+            Console.WriteLine("This is a Jetta");
+        }
+    }
+}
