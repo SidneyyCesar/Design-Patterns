09ca6e2fc89c6b7105e8752cb256a57049c82177
Sidney Cesar <scesar@bcompany.com.br>
2018-12-20 13:56:36 -0200

    Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Sourdough sourdough = new Sourdough();
+                sourdough.Make();
+
+                TwelveGrain twelveGrain = new TwelveGrain();
+                twelveGrain.Make();
+
+                WholeWheat wholeWheat = new WholeWheat();
+                wholeWheat.Make();
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
